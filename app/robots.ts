3bca9ffa5be9251f671aa3e765eefef85513f5be
JiaMing Ding <djmfrontend@163.com>
2026-05-10 20:21:38 +0800

    add sitemap and robots

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://my-first-site.vercel.app/sitemap.xml",
+  };
+}
