28dbf4cd1d578827c66eee3f96019d5b46a38be0
JiaMing Ding <djmfrontend@163.com>
2026-05-10 21:56:52 +0800

    添加谷歌广告代码

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -16,7 +17,7 @@ export const metadata = {
   title: "Free JSON Formatter Online",
   description:
     "Format, validate and beautify JSON instantly with our free online JSON formatter tool.",
-  metadataBase: new URL("https://my-first-site-six-nu.vercel.app"),
+  metadataBase: new URL("https://cainiaodjm.top"),
 };
 export default function RootLayout({
   children,
@@ -28,6 +29,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7266064919631581"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://my-first-site-six-nu.vercel.app/sitemap.xml",
+    sitemap: "https://cainiaodjm.top/sitemap.xml",
   };
 }
